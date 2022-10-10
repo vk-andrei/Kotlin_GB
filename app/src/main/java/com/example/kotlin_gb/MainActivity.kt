51227098476356eff279c56c4d56a64fe412b33e
vk-an <vk-andrei@mail.ru>
2022-10-10 20:13:12 +0300

    Lesson 1 done

--- app/src/main/java/com/example/kotlin_gb/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_gb/MainActivity.kt
@@ -16,6 +16,14 @@ class MainActivity : AppCompatActivity() {
 
         setListeners()
         setPersons()
+
+        // создаем сущность сразу без  объявления класса
+        val andrei = object {
+            val name = "andrei"
+            var age = 39
+        }
+        andrei.age = 40
+
     }
 
     private fun setListeners() {
@@ -29,7 +37,12 @@ class MainActivity : AppCompatActivity() {
         personList.add(Person("Ivan", 21, 3))
         personList.add(Person("Niko", 20, 2))
         personList.add(Person("Mark", 39, 1))
+
+        val personNew = personList[2].copy(name = "Vladimir")
+        personList.add(personNew)
+
+        for (person in personList) {
+            Log.d("TAG", "$person")
+        }
     }
-
-
 }
