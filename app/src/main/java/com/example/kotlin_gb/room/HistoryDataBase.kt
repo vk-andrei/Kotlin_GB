99c4669a4e15ce76078efe0b4543231cafcf99b2
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-11-22 19:49:23 +0300

    Lesson 8. Create package Room: - HistoryEntity.kt - HistoryDao.kt (interface) - HistoryDataBase.kt

---
+++ app/src/main/java/com/example/kotlin_gb/room/HistoryDataBase.kt
@@ -0,0 +1,17 @@
+package com.example.kotlin_gb.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+// Это абстрактный класс, у которого должна быть определена аннотация @Database со всеми
+//табличными объектами. В нашем случае это массив всего с одной таблицей. Версию БД указываем в
+//version. Обязательно увеличиваем версию при изменении структуры БД. Параметр exportSchema
+//указывает, создавать или нет файл в проекте, где будет храниться история версий БД. Это
+//совершенно необязательно, но в коммерческих проектах — практика хорошая. В классе также
+//требуется определить метод, возвращающий объект доступа к данным. У нас это метод historyDao.
+
+@Database(entities = arrayOf(HistoryEntity::class), version = 1, exportSchema = false)
+abstract class HistoryDataBase : RoomDatabase() {
+
+    abstract fun historyDao(): HistoryDao
+}
