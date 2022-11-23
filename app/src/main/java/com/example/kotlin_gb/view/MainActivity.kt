c53a72c97ca53c3c6560e9287aa1b6c17347cadc
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-11-23 17:14:54 +0300

    Lesson 8. Create bottom_menu.xml Add 2 items (Home and History) Edit main activity Edit gradle (for navigation)

--- app/src/main/java/com/example/kotlin_gb/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_gb/view/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.kotlin_gb.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_gb.R
+import com.example.kotlin_gb.view.history.HistoryFragment
 import com.example.kotlin_gb.view.weatherlist.WeatherListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemReselectedListener{
+            when (it.itemId) {
+                R.id.Home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, WeatherListFragment.newInstance()).commit()
+                }
+                R.id.History -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, HistoryFragment.newInstance()).commit()
+                }
+            }
+        }
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
--- app/src/main/java/com/example/kotlin_gb/view/history/HistoryFragment.kt
+++ app/src/main/java/com/example/kotlin_gb/view/history/HistoryFragment.kt
@@ -1,26 +1,13 @@
 package com.example.kotlin_gb.view.history
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.kotlin_gb.R
 
 class HistoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-/*
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-*/
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,15 +18,7 @@ class HistoryFragment : Fragment() {
     }
 
     companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-/*
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-*/
-                }
-            }
+        fun newInstance() = HistoryFragment()
     }
+
 }
