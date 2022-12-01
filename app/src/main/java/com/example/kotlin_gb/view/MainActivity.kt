a9231467fc4b1ba2dffc1f8da44c98f691b35c3e
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-12-01 09:38:51 +0300

    Lesson 9. HW 9. Add menu Contacts on Main Create ContentProviderForContactsFragment.kt and its XML.

--- app/src/main/java/com/example/kotlin_gb/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_gb/view/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.kotlin_gb.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.replace
 import com.example.kotlin_gb.R
+import com.example.kotlin_gb.view.contacts.ContentProviderForContactsFragment
 import com.example.kotlin_gb.view.history.HistoryFragment
 import com.example.kotlin_gb.view.weatherlist.WeatherListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -25,9 +27,13 @@ class MainActivity : AppCompatActivity() {
                 R.id.History -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.container, HistoryFragment.newInstance())
-                        .addToBackStack("")
-                        .commitAllowingStateLoss()
+                        .addToBackStack("").commitAllowingStateLoss()
                     return@setOnNavigationItemSelectedListener true
+                }
+                R.id.Contacts -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, ContentProviderForContactsFragment.newInstance())
+                        .addToBackStack("").commitAllowingStateLoss()
                 }
             }
             return@setOnNavigationItemSelectedListener true
