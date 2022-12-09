6aea82792e20907325658c6302b1d5d24b3e6cfe
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-12-09 21:10:15 +0300

    Lesson 11. HW11. Make Notification in MainActivity.kt
    private fun pushNotification

--- app/src/main/java/com/example/kotlin_gb/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_gb/view/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.kotlin_gb.view
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.example.kotlin_gb.R
+import com.example.kotlin_gb.utils.Const.Companion.CHANNEL_HIGH_PRIORITY_ID
+import com.example.kotlin_gb.utils.Const.Companion.NOTIFICATION_ID
 import com.example.kotlin_gb.view.contacts.ContactsFragment
 import com.example.kotlin_gb.view.history.HistoryFragment
 import com.example.kotlin_gb.view.maps.MapsFragment
@@ -48,5 +56,33 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, WeatherListFragment.newInstance()).commit()
         }
+
+        // NOTIFICATION:
+        pushNotification("Attention!", "We make new feature in our APP. Open and you will see!")
+    }
+
+    private fun pushNotification(title: String, body: String) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification = NotificationCompat.Builder(this, CHANNEL_HIGH_PRIORITY_ID).apply {
+            setContentTitle(title)
+            setContentText(body)
+            setSmallIcon(R.drawable.ic_marker_google_map)
+            priority = NotificationCompat.PRIORITY_MAX
+        }.build()
+
+        // После 26 версии доступно НЕСКОЛЬКО каналов для ПУШЕЙ (пользователь может отключать или все или некоторые)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelHigh = NotificationChannel(
+                CHANNEL_HIGH_PRIORITY_ID, "CHANNEL HIGH", NotificationManager.IMPORTANCE_HIGH
+            )
+            channelHigh.description = "Channel for importance notifications"
+            notificationManager.createNotificationChannel(channelHigh)
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        } else {
+            // до 26 версии ТОЛЬКО один канал для пушей:
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
+
     }
 }
